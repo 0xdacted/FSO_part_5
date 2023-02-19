1b6ebed9b8a42e0569d10eb10166376d3a61ea42
Red <crabmarket56@gmail.com>
2023-02-19 15:59:56 +0900

    Add login functions

--- src/App.js
+++ src/App.js
@@ -1,15 +1,62 @@
 import { useState, useEffect } from 'react'
 import Blog from './components/Blog'
 import blogService from './services/blogs'
+import loginService from './services/login'
 
 const App = () => {
+  const [errorMessage, setErrorMessage] = useState(null)
   const [blogs, setBlogs] = useState([])
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [user, setUser] = useState(null)
+
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
       setBlogs( blogs )
     )  
   }, [])
+
+  const handleLogin = async (event) => {
+    event.preventDefault()
+    try {
+      const user = await loginService.login({
+        username, password
+      })
+      window.localStorage.setItem(
+        'loggedNoteappUser', JSON.stringify(user)
+      )
+      blogService.setToken(user.token)
+      setUser(user)
+      setUsername('')
+      setPassword('')
+    } catch (exception) {
+      setErrorMessage('Wrong credentials')
+      setTimeout(() => {
+        setErrorMessage(null)
+      }, 5000)
+    }
+    console.log('logging in with', username, password)
+  }
+
+  const loginForm = () => {
+    <form onSubmit={handleLogin}>
+        <div>
+          username
+          <input type='text' value={username} name='Username' 
+          onChange={({ target }) => setUsername(target.value)} 
+          />
+        </div>
+        <div>
+          password
+          <input type='password' value={password} name='Password'
+          onChange={({ target }) => setPassword(target.value)}
+          />
+          <button type="submit">login</button>
+        </div>
+      </form>
+  }
+
 
   return (
     <div>
