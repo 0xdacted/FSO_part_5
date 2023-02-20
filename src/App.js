d6e665a4831e410ca56f1b63c7cca6306e95e6f0
Red <crabmarket56@gmail.com>
2023-02-20 13:47:17 +0900

    Fix input bug

--- src/App.js
+++ src/App.js
@@ -118,8 +118,8 @@ const App = () => {
     <div>
     <h2>create new</h2>
     <form onSubmit={addBlog}>
-      <div>title: <input type='text' value={newBlog.title} name='Title' onChange={handleBlogChange}/></div>
-      <div>author: <input type='text' value={newBlog.author} name='Author' onChange={handleBlogChange}/></div>
+      <div>title: <input type='text' value={newBlog.title} name='title' onChange={handleBlogChange}/></div>
+      <div>author: <input type='text' value={newBlog.author} name='author' onChange={handleBlogChange}/></div>
       <div>url: <input type='text' value={newBlog.url} name='url' onChange={handleBlogChange}/></div>
       <button type="submit">create</button>
     </form>
