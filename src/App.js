d406e9d97e18692ee2381c6538de7667d10a54b5
Red <crabmarket56@gmail.com>
2023-02-20 13:43:14 +0900

    Draft blog form and handling

--- src/App.js
+++ src/App.js
@@ -9,6 +9,12 @@ const App = () => {
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
   const [user, setUser] = useState(null)
+  const [newBlog, setNewBlog] = useState({
+    title: '',
+    author: '',
+    url: '',
+    user: null,
+  })
 
 
   useEffect(() => {
@@ -79,6 +85,49 @@ const App = () => {
     )
   }
 
+  const addBlog = (event) => {
+    event.preventDefault()
+    const blogObject = {
+      title: newBlog.title,
+      author: newBlog.author,
+      url: newBlog.url,
+      user: user._id
+    }
+    blogService
+    .create(blogObject).then(returnedBlog => {
+      setBlogs(blogs.concat(returnedBlog))
+      setNewBlog({ 
+        title: '',
+        author: '',
+        url: '',
+        user: null, 
+      })
+    })
+    
+  }
+  const handleBlogChange = (event) => {
+    const { name, value } = event.target
+    setNewBlog(prevState => ({
+      ...prevState,
+      [name]: value
+    }))
+  }
+
+  const blogForm = () => {
+    return (
+    <div>
+    <h2>create new</h2>
+    <form onSubmit={addBlog}>
+      <div>title: <input type='text' value={newBlog.title} name='Title' onChange={handleBlogChange}/></div>
+      <div>author: <input type='text' value={newBlog.author} name='Author' onChange={handleBlogChange}/></div>
+      <div>url: <input type='text' value={newBlog.url} name='url' onChange={handleBlogChange}/></div>
+      <button type="submit">create</button>
+    </form>
+    </div>
+    )
+  }
+
+
 
   return (
     <div>
@@ -87,7 +136,8 @@ const App = () => {
       
       <h2>blogs</h2>
       {user && <div>
-        <p>{user.name} logged in {logoutButton()}</p>      
+        <p>{user.name} logged in {logoutButton()}</p>
+        <div>{blogForm()}</div>      
         {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
