ebd9ac5681373f495a2c33eb9d3c4696b3fd0869
Red <crabmarket56@gmail.com>
2023-03-02 14:33:15 +0900

    Instantiate Notification component

--- src/components/Notification.js
+++ src/components/Notification.js
@@ -1,2 +1,17 @@
 import React from 'react'
 import { connect } from 'react-redux'
+
+const Notification = ({ message, isSuccess }) => {
+  if (message === null) {
+    return null
+  }
+  const className = isSuccess ? 'success' : 'failure'
+  return <div className={`notification ${className}`}>{message} </div>
+}
+
+const mapStateToProps = (state) => ({
+  message: state.notification.message,
+  isSuccess: state.notification.isSuccess,
+})
+
+export default connect(mapStateToProps)(Notification)
