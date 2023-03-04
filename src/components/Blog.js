89ddb001ac9ea91b036fb6ceb6e03864cd42b079
Red <crabmarket56@gmail.com>
2023-03-04 11:16:47 +0900

    Add add comment form

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,9 +1,26 @@
 import { Link } from "react-router-dom"
+import { useState } from "react"
+import { useDispatch } from "react-redux"
+import { addCommment } from '../reducers/blogReducer'
 
-const Blog = ({ blog }) => (
-  <div>
-   <Link to={`/blogs/${blog.id}`}>{blog.title}</Link> {blog.author}
-  </div>
-)
+const Blog = ({ blog }) => {
+  const dispatch = useDispatch()
+  const [commentText, setCommentText] = useState('')
+  const handleCommentSubmit = async (event) => {
+    event.preventDefault()
+    dispatch(addComment(blog.id, commentText))
+    setCommentText('')
+  }
+  return (
+    <div>
+    <Link to={`/blogs/${blog.id}`}>{blog.title}</Link> {blog.author}
+    <h3>comments</h3>
+    <form onSubmit={handleCommentSubmit}>
+      <input type="text" value={commentText} onChange={(event) => setCommentText(event.target.value)} />
+      <button type="submit">add comment</button>
+    </form>
+    </div>
+  )
+  }
 
 export default Blog
